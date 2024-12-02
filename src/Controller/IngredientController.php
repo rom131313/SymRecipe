13fe2e90ea01b96f8ea4afed930d022ae584c2af
alse <halison.andria@gmail.com>
2024-12-02 19:37:38 +0100

    ✨  CRUD - delete  Ingrédient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -7,11 +7,13 @@ use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class IngredientController extends AbstractController
 {
@@ -72,14 +74,51 @@ class IngredientController extends AbstractController
     }
 
     #[Route('/ingredient/edition/{id}', 'ingredient.edit', methods: ['GET', 'POST'])]
-    public function edit(IngredientRepository $repository, int $id) : Response
+    // public function edit(IngredientRepository $repository, int $id) : Response
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager) : Response
     {
-        $ingredient = $repository->findOneBy(["id"=> $id]);
+        // $ingredient = $repository->findOneBy(["id"=> $id]);
         $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient); //commit dans bdd
+            $manager->flush(); //push
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été modifié avec succès !'
+            );
+
+            return $this->redirectToRoute('ingredient.index');
+        }
 
         return $this->render('pages/ingredient/edit.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/ingredient/suppression/{id}', 'ingredient.delete', methods: ['GET'])]
+    public function delete(EntityManagerInterface $manager, Ingredient $ingredient): Response 
+    {   
+        if(!$ingredient) {
+        //     $this->addFlash(
+        //         'error',
+        //         'L\'ingrédient en question n\'a pas été trouvé !'
+        //     );
+        // }
+
+        $manager->remove($ingredient);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'Votre ingrédient a été supprimé avec succès !'
+        );
+
+        return $this->redirectToRoute('ingredient.index');
     }
 
 }
